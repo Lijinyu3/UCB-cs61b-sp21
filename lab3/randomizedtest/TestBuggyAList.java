afc24f56670b5f7fc9fba13b3e21f9455ebec2e1
Lijinyu3 <enotaa33@gmail.com>
2023-03-30 21:37:03 +0800

    lab03: complete simple comparison test

--- lab3/randomizedtest/TestBuggyAList.java
+++ lab3/randomizedtest/TestBuggyAList.java
@@ -8,5 +8,18 @@ import static org.junit.Assert.*;
  * Created by hug.
  */
 public class TestBuggyAList {
-  // YOUR TESTS HERE
+    @Test
+    public void testThreeAddThreeRemove() {
+        BuggyAList<Integer> buggyAList = new BuggyAList<>();
+        AListNoResizing<Integer> aListNoResizing = new AListNoResizing<>();
+        int[] nums = new int[] {4, 5, 6};
+        for (int i : nums) {
+            buggyAList.addLast(i);
+            aListNoResizing.addLast(i);
+        }
+        assertEquals(aListNoResizing.size(), buggyAList.size());
+        for (int i = 0; i < nums.length; i++) {
+            assertEquals(aListNoResizing.removeLast(), buggyAList.removeLast());
+        }
+    }
 }
