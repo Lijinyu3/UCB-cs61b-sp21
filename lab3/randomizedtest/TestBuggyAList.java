d96b3d6f91a081b4cd10fbcb80464d786f7f83f6
Lijinyu3 <enotaa33@gmail.com>
2023-03-30 21:45:06 +0800

    lab03: complete randomizedTest

--- lab3/randomizedtest/TestBuggyAList.java
+++ lab3/randomizedtest/TestBuggyAList.java
@@ -22,4 +22,33 @@ public class TestBuggyAList {
             assertEquals(aListNoResizing.removeLast(), buggyAList.removeLast());
         }
     }
+
+
+    @Test
+    public void randomizedTest() {
+        AListNoResizing<Integer> L = new AListNoResizing<>();
+
+        int N = 500;
+        for (int i = 0; i < N; i += 1) {
+            int operationNumber = StdRandom.uniform(0, 4);
+            if (operationNumber == 0) {
+                // addLast
+                int randVal = StdRandom.uniform(0, 100);
+                L.addLast(randVal);
+                System.out.println("addLast(" + randVal + ")");
+            } else if (operationNumber == 1) {
+                // size
+                int size = L.size();
+                System.out.println("size: " + size);
+            } else if (operationNumber == 2 && L.size() > 0) {
+                // removeLast
+                int removedItem = L.removeLast();
+                System.out.println("removed last: " + removedItem);
+            } else if (operationNumber == 3 && L.size() > 0) {
+                // getLast
+                int lastItem = L.getLast();
+                System.out.println("last item: " + lastItem);
+            }
+        }
+    }
 }
